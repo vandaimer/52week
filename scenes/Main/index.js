743eba3da50c89a4cb6dfacaa60bd714d64e1a3e
Luiz Filipe <moresco.luiz@gmail.com>
2018-02-03 11:54:37 -0200

    First version of bottom nav bar

--- scenes/Main/index.js
+++ scenes/Main/index.js
@@ -1,20 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View
+} from 'react-native';
 import WeekList from './components/WeekList';
 
-export default class Main extends Component<{}> {
+export default class Main extends Component {
 
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <WeekList/>
-      </View>
-    );
-  }
+	render() {
+	  return (
+	    <View style={styles.main}>
+	      <WeekList/>
+	      <View style={styles.mainBottomNavBar}>
+	       	<View style={styles.contentBottonNavBar}>
+	      		<Text style={styles.text}>Depositado R$ 100,00</Text>
+	      		<Text style={styles.text}>Meta R$ 100,00</Text>
+	      	</View>
+	      </View>
+	    </View>
+	  );
+	}
 }
+
+const styles = StyleSheet.create({
+	main: {
+		flex: 1
+	},
+	mainBottomNavBar : {
+		height: 75
+	},
+	contentBottonNavBar: {
+		flex: 1,
+		flexDirection: 'row',
+		margin: 16,
+		alignItems: 'center',
+		justifyContent: 'space-around'
+	},
+	text: {
+		fontWeight: 'bold'
+	}
+});
