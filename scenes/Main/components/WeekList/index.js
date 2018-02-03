0a0334c600aee6c5e525602095117ec04cc111c5
Luiz Filipe <moresco.luiz@gmail.com>
2018-02-03 11:34:05 -0200

    Improve WeekList component to removing unnecessary code

--- scenes/Main/components/WeekList/index.js
+++ scenes/Main/components/WeekList/index.js
@@ -1,16 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
-  Text,
   View,
-  Button,
   ScrollView
 } from 'react-native';
 import R from 'ramda';
@@ -61,32 +52,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  text: {
-    fontSize: 20,
-    margin: 16,
-    padding: 16,
-  },
-  itemList: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    margin: 16,
-    borderBottomWidth: 1,
-  },
-  itemListDeposited: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    margin: 16,
-    borderBottomWidth: 1,
-    backgroundColor: 'green'
-  },
-  command: {
-    width: 100,
-    margin: 16,
-    height: 35,
-    marginTop: 25,
-    alignItems: "center",
   }
 });
