e83b4c515bf3b05600de955fdfe380aaa72229fd
Luiz Filipe <moresco.luiz@gmail.com>
2018-02-06 11:18:39 -0200

    Improve reducer file to map the actions

--- scenes/Main/components/reducer.js
+++ scenes/Main/components/reducer.js
@@ -1,7 +1,9 @@
 export default function totalDeposited(state = 0, action) {
 	switch(action.type) {
-		case 'NEW_DEPOIST':
+		case 'ADD_TO_SAVINGS_ACCOUNT_AMOUNT':
 			return (state + action.value);
+		case 'REMOVE_FROM_SAVINGS_ACCOUNT_AMOUNT':
+			return (state - action.value);
 		default:
 			return state;
 	}
