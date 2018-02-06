b07f0550004c62ba35ecfe517d93c3e655693b26
Luiz Filipe <moresco.luiz@gmail.com>
2018-02-06 11:16:01 -0200

    Remove debug

--- scenes/Main/components/reducer.js
+++ scenes/Main/components/reducer.js
@@ -1,10 +1,8 @@
 export default function totalDeposited(state = 0, action) {
 	switch(action.type) {
 		case 'NEW_DEPOIST':
-			console.log('NEW DEPOSIT', (state + action.value));
 			return (state + action.value);
 		default:
-			console.log('DEFAULT STATE');
 			return state;
 	}
 };
