c46bfb25e618790b319868369d9733d506331ca9
Luiz Filipe <moresco.luiz@gmail.com>
2018-10-05 22:36:19 -0300

    Implement the reducers and actions

--- reducers.js
+++ reducers.js
@@ -1,14 +1,29 @@
 const initialState = {
   totalSavingsAmount: 0,
   savingsAccountInfo: 0,
+  savingsPercentage: 0,
 };
 
+const calculateSavingsPercentage = (totalSavingsAmount, savingsAccountInfo) => ((totalSavingsAmount * 100) / savingsAccountInfo).toFixed(1);
+
 const actions = () => ({
-  maxSavingsAccoutAmount: () => {},
-  addToSavingsAccount: () => {},
-  removeFromSavingsAccount: () => {},
+  maxSavingsAccoutAmount: (state, value) => ({ ...state, savingsAccountInfo: value }),
+  addToSavingsAccount: (state, value) => {
+    let { totalSavingsAmount, savingsAccountInfo } = state;
+    totalSavingsAmount += value;
+
+    const savingsPercentage = calculateSavingsPercentage(totalSavingsAmount, savingsAccountInfo);
+    return { totalSavingsAmount, savingsPercentage };
+  },
+  removeFromSavingsAccount: (state, value) => {
+    let { totalSavingsAmount, savingsAccountInfo } = state;
+    totalSavingsAmount -= value;
+
+    const savingsPercentage = calculateSavingsPercentage(totalSavingsAmount, savingsAccountInfo);
+    return { totalSavingsAmount, savingsPercentage };
+  },
 });
 
-const props = ['savingsAccountInfo', 'totalSavingsAmount'];
+const props = ['savingsAccountInfo', 'totalSavingsAmount', 'savingsPercentage'];
 
 export { initialState, props, actions };
